f4f68e69b2c1d52b171bd57ce01cb611a6e90cdd
Andy Ganse <andy@ganse.org>
2022-10-16 10:55:58 -0700

    tweaked left-icon carat to cyan to match local-version

--- scripts/dracula.sh
+++ scripts/dracula.sh
@@ -116,10 +116,10 @@ main()
 
   # Status left
   if $show_powerline; then
-    tmux set-option -g status-left "#[bg=${green},fg=${dark_gray}]#{?client_prefix,#[bg=${yellow}],} ${left_icon} #[fg=${green},bg=${gray}]#{?client_prefix,#[fg=${yellow}],}${left_sep}"
+    tmux set-option -g status-left "#[bg=${green},fg=${dark_gray}]#{?client_prefix,#[bg=${cyan}],} ${left_icon} #[fg=${green},bg=${gray}]#{?client_prefix,#[fg=${cyan}],}${left_sep}"
     powerbg=${gray}
   else
-    tmux set-option -g status-left "#[bg=${green},fg=${dark_gray}]#{?client_prefix,#[bg=${yellow}],} ${left_icon}"
+    tmux set-option -g status-left "#[bg=${green},fg=${dark_gray}]#{?client_prefix,#[bg=${cyan}],} ${left_icon}"
   fi
 
   # Status right
